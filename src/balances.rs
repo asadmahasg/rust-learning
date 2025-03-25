6b07ae21be869bbe47add6c6f13936905b29cb15
Asad Mahmood Ashgar <asad.mahmood.official1@gmail.com>
2025-03-25 12:13:40 +0500

    Added system pallet

--- src/balances.rs
+++ src/balances.rs
@@ -1,7 +1,12 @@
 use std::collections::BTreeMap;
 
-pub struct Pallet{
-    balances: BTreeMap<String, u128>,
+/// This is the Balances Module.
+/// It is a simple module which keeps track of how much balance each account has in this state
+/// machine.
+/* TODO: Add the derive macro to implement the `Debug` trait for `Pallet`. */
+pub struct Pallet {
+	// A simple storage mapping from accounts (`String`) to their balances (`u128`).
+	balances: BTreeMap<String, u128>,
 }
 
 impl Pallet {
@@ -20,14 +25,58 @@ impl Pallet {
 	pub fn balance(&self, who: &String) -> u128 {
 		*self.balances.get(who).unwrap_or(&0)
 	}
+
+	/// Transfer `amount` from one account to another.
+	/// This function verifies that `from` has at least `amount` balance to transfer,
+	/// and that no mathematical overflows occur.
+	pub fn transfer(
+		&mut self,
+		caller: String,
+		to: String,
+		amount: u128,
+	) -> Result<(), &'static str> {
+		let caller_balance = self.balance(&caller);
+		let to_balance = self.balance(&to);
+
+		let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Not enough funds.")?;
+		let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+
+		self.balances.insert(caller, new_caller_balance);
+		self.balances.insert(to, new_to_balance);
+
+		Ok(())
+	}
 }
 
-#[test]
-fn init_balances() {
-	let mut balances = super::Pallet::new();
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn init_balances() {
+		let mut balances = super::Pallet::new();
 
-	assert_eq!(balances.balance(&"alice".to_string()), 0);
-	balances.set_balance(&"alice".to_string(), 100);
-	assert_eq!(balances.balance(&"alice".to_string()), 100);
-	assert_eq!(balances.balance(&"bob".to_string()), 0);
+		assert_eq!(balances.balance(&"alice".to_string()), 0);
+		balances.set_balance(&"alice".to_string(), 100);
+		assert_eq!(balances.balance(&"alice".to_string()), 100);
+		assert_eq!(balances.balance(&"bob".to_string()), 0);
+	}
+
+	#[test]
+	fn transfer_balance() {
+		let mut balances = super::Pallet::new();
+
+		assert_eq!(
+			balances.transfer("alice".to_string(), "bob".to_string(), 51),
+			Err("Not enough funds.")
+		);
+
+		balances.set_balance(&"alice".to_string(), 100);
+		assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 51), Ok(()));
+		assert_eq!(balances.balance(&"alice".to_string()), 49);
+		assert_eq!(balances.balance(&"bob".to_string()), 51);
+
+		assert_eq!(
+			balances.transfer("alice".to_string(), "bob".to_string(), 51),
+			Err("Not enough funds.")
+		);
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 mod balances;
+mod system;
+
+pub struct Runtime {
+	/* TODO:
+		- Create a field `system` which is of type `system::Pallet`.
+		- Create a field `balances` which is of type `balances::Pallet`.
+	*/
+}
+
+impl Runtime {
+	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
+	fn new() -> Self {
+		/* TODO: Create a new `Runtime` by creating new instances of `system` and `balances`. */
+		unimplemented!()
+	}
+}
 
 fn main() {
-    println!("Hello, world!");
-    println!()
+	println!("Hello, world!");
 }
